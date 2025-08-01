3538978fa1c17177e2b01874f3246ea7bd495326
kaeclayton <kclayton@byupathway.edu>
2025-08-01 11:39:52 -0700

    product review webform

---
+++ scripts/form.js
@@ -0,0 +1,61 @@
+const products = [
+    {
+        id: "fc-1888",
+        name: "flux capacitor",
+        averagerating: 4.5
+    },
+    {
+        id: "fc-2050",
+        name: "power laces",
+        averagerating: 4.7
+    },
+    {
+        id: "fs-1987",
+        name: "time circuits",
+        averagerating: 3.5
+    },
+    {
+        id: "ac-2000",
+        name: "low voltage reactor",
+        averagerating: 3.9
+    },
+    {
+        id: "jj-1969",
+        name: "warp equalizer",
+        averagerating: 5.0
+    }
+];
+
+document.addEventListener('DOMContentLoaded', function () {
+    const select = document.querySelector('select[name="product"');
+
+    products.forEach(product => {
+        const option = document.createElement('option');
+        option.value = product.id;
+        option.textContent = product.name;
+        select.appendChild(option);
+    });
+});
+
+const copyrightYear = document.querySelector('#currentyear');
+const lastModifiedElement = document.querySelector('#lastModified');
+
+if (copyrightYear) {
+    const currentYear = new Date().getFullYear();
+    copyrightYear.textContent = currentYear;
+}
+
+if (lastModifiedElement) {
+    lastModifiedElement.textContent = `Last Modified: ${formatDate(document.lastModified)}`;
+}
+
+function formatDate(dateString) {
+    const options = {
+        year: 'numeric',
+        month: 'long',
+        day: 'numeric',
+        hour: '2-digit',
+        minute: '2-digit'
+    };
+    return new Date(dateString).toLocaleDateString('en-US', options);
+}
